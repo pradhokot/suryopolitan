978647510f110f3a15a6e9af04b78e7fd7c740ad
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-02-20 10:36:23 +0700

    add shadow on card 1

--- card1.js
+++ card1.js
@@ -1,5 +1,5 @@
 document.write(`
-<div class="card">
+<div class="card shadow">
    <img src="assets/img/portrait.png" class="card-img-top rounded" alt="">
    <div class="card-img-overlay text-capitalize text-light top-unset">
       <h4 class="card-title font-weight-bold">mahir machines learning</h4>
