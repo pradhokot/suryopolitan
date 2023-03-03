50f31bc6ffe68ac148767442542cdf7a0aeb5268
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-03-03 11:29:16 +0700

    add swiper js on detail page (pemateri section)

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -38,6 +38,7 @@
 // ============================ slider product
 var swiper = new Swiper(".slider-produk", {
    slidesPerView: 1,
+   spaceBetween: 15,
    autoplay: {
       delay: 2500,
       disableOnInteraction: false
@@ -51,14 +52,32 @@ var swiper = new Swiper(".slider-produk", {
    breakpoints: {
       576: {
          slidesPerView: 2,
-         spaceBetween: 15,
       },
       992: {
          slidesPerView: 3,
-         spaceBetween: 15,
       },
       1200: {
          slidesPerView: 4,
+      },
+   },
+});
+
+// ============================ slider pemateri
+var swiper = new Swiper("#slider-pemateri", {
+   slidesPerView: 1,
+   rewind: true,
+   rewind: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   breakpoints: {
+      576: {
+         slidesPerView: 2,
+         spaceBetween: 30,
+      },
+      1200: {
+         slidesPerView: 3,
          spaceBetween: 15,
       },
    },
